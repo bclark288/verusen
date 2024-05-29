08b99ef8bfdc2f174aba60db7a6cc3bb034674c1
bclark288 <bclark288@gmail.com>
2024-05-29 11:39:16 -0400

    add load to sql

---
+++ load_iris_db.py
@@ -0,0 +1,8 @@
+import sqlite3
+import pandas as pd
+
+con = sqlite3.connect('./database.sqlite')
+cur = con.cursor()
+df = pd.read_csv('./Iris.csv')
+df.to_sql('Iris', con, if_exists='append', index=False)
+con.commit()
